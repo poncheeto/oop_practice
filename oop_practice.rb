db7f390c5f38340f024e1e36b9c28ed5ec4af230
Poncho <jplyles1212@gmail.com>
2020-12-06 16:05:01 -0800

    Practice using instance variables and methods

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,46 +1,15 @@
 # good_dog.rb
 
-module Speak
-  def speak(sound)
-    puts sound
-  end
-end
-
 class GoodDog
-  include Speak
-end
-
-class HumanBeing
-  include Speak
-end
-
-sparky = GoodDog.new
-sparky.speak("Arf!")
-bob = HumanBeing.new
-bob.speak("Hello!")
-
-puts "---GoodDog ancestors---"
-puts GoodDog.ancestors
-puts ''
-puts "---HumanBeing ancestors---"
-puts HumanBeing.ancestors
-
-# How to create object in Ruby
-class Dog
-end
-
-mimi = Dog.new
-
-# How to create a module and include it properly
-module Speak
-  def speak(sound)
-    puts sound
+  def initialize(name)
+    @name = name
   end
+
+  def speak
+    "Arf!"
+  end
+  
 end
 
-class Dog
-  include Speak
-end
-
-mimi = Dog.new
-mimi.speak("Yap")
+sparky = GoodDog.new("Sparky"
+  sparky.speak
