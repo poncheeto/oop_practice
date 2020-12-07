582cef305749cdaf863aeefbb99c197f17c8f7ab
Poncho <jplyles1212@gmail.com>
2020-12-07 14:42:55 -0800

    Practice using constants

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,19 +1,13 @@
 class GoodDog
-  @@number_of_dogs = 0
+  DOG_YEARS = 7
 
-  def initialize
-    @@number_of_dogs += 1
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age = a * DOG_YEARS
   end
-
-  def self.total_number_of_dogs
-    @@number_of_dogs
-  end
-
 end
 
-puts GoodDog.total_number_of_dogs
-
-dog1 = GoodDog.new
-dog2 = GoodDog.new
-
-puts GoodDog.total_number_of_dogs
+sparky = GoodDog.new('Sparky', 4)
+p sparky.age
