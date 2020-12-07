54a00f45ffb0066feca4a1b0ee5f6bcd694f3700
Poncho <jplyles1212@gmail.com>
2020-12-07 14:37:44 -0800

    Demonstrate how to use class variable/method to track class-level details

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,7 +1,19 @@
 class GoodDog
-  def self.what_am_i
-    "I'm a GoodDog class!"
+  @@number_of_dogs = 0
+
+  def initialize
+    @@number_of_dogs += 1
   end
+
+  def self.total_number_of_dogs
+    @@number_of_dogs
+  end
+
 end
 
-GoodDog.what_am_i
+puts GoodDog.total_number_of_dogs
+
+dog1 = GoodDog.new
+dog2 = GoodDog.new
+
+puts GoodDog.total_number_of_dogs
