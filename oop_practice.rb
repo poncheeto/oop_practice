3be0c76bf5253e551965fe81fbf0cb094009a59b
Poncho <jplyles1212@gmail.com>
2020-12-10 14:00:05 -0800

    Practice using private methods

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,20 +1,23 @@
-module Mammal
-  class Dog
-    def speak(sound)
-      p "#{sound}"
-    end
+class GoodDog
+  DOG_YEARS = 7
+
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    self.name = n
+    self.age = a
   end
 
-  class Cat
-    def say_name(name)
-      p "#{name}"
-    end
+  def public_disclosure
+    "#{self.name} in human years is #{human_years}"
   end
 
-  def self.some_out_of_place_method(num)
-    num ** 2
+  private
+
+  def human_years
+    age * DOG_YEARS
   end
 end
 
-value = Mammal.some_out_of_place_method(4)
-value2 = Mammal::some_out_of_place_method(4)
+sparky = GoodDog.new('Sparky', 4)
+p sparky.public_disclosure
