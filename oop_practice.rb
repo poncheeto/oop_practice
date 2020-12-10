b3f8905dee8dec17916e8666f0eab1d95e02252e
Poncho <jplyles1212@gmail.com>
2020-12-10 14:08:10 -0800

    Practice using protected keyword

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,23 +1,11 @@
-class GoodDog
-  DOG_YEARS = 7
-
-  attr_accessor :name, :age
-
-  def initialize(n, a)
-    self.name = n
-    self.age = a
+class Animal
+  def a_public_method
+    "Will this work?" + self.a_protected_method
   end
 
-  def public_disclosure
-    "#{self.name} in human years is #{human_years}"
-  end
+  protected
 
-  private
-
-  def human_years
-    age * DOG_YEARS
+  def a_protected_method
+    "Yes, I'm protected!"
   end
 end
-
-sparky = GoodDog.new('Sparky', 4)
-p sparky.public_disclosure
