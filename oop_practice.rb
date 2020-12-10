7a43cb13540a534aafe39ccb7d2a5d7e6d536b86
Poncho <jplyles1212@gmail.com>
2020-12-09 17:25:33 -0800

    Use modules namespacing

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,31 +1,18 @@
-module Walkable
-  def walk
-    "I'm walking."
+module Mammal
+  class Dog
+    def speak(sound)
+      p "#{sound}"
+    end
+  end
+
+  class Cat
+    def say_name(name)
+      p "#{name}"
+    end
   end
 end
 
-module Swimmable
-  def swim
-  end
-end
-
-module Climbable
-  def climb
-  end
-end
-
-class Animal
-  include Walkable
-
-  def speak
-    "I'm an animal, and I speak!"
-  end
-end
-
-class GoodDog < Animal
-  include Swimmable
-  include Climbable
-end
-
-puts "---GoodDog method lookup---"
-puts GoodDog.ancestors
+buddy = Mammal::Dog.new
+kitty = Mammal::Cat.new
+buddy.speak('Arf!')
+kitty.say_name('Kitty')
