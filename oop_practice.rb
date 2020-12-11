1a6cb3ac3e22a7de491e8d076a4a7f767417018e
Poncho <jplyles1212@gmail.com>
2020-12-10 17:01:51 -0800

    Add superclass to inherit from for MyCar and MyTruck classes

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,20 +1,39 @@
-class Parent
-  def say_hi
-    p "Hi from Parent."
+class Vehicle
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
   end
 end
 
-class Child < Parent
-  def say_hi
-    p "Hi from Child"
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def initialize(year, model, color)
+    @year = year
+    @model = model
+    @color = color
+    @current_speed = 0
   end
 
-  def send
-    p "send from Child..."
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  class myTruck < Vehicle
+    NUMBER_OF_DOORS = 2
   end
 end
-
-son = Child.new
-son.send :say_hi
-lad = Child.new
-las.send :say_hi
