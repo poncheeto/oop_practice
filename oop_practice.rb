f8a42b90a2b3053d7f934d392a11a1d3ed66b552
Poncho <jplyles1212@gmail.com>
2020-12-10 16:45:19 -0800

    Explore accidental method overriding

--- oop_practice.rb
+++ oop_practice.rb
@@ -8,7 +8,13 @@ class Child < Parent
   def say_hi
     p "Hi from Child"
   end
+
+  def send
+    p "send from Child..."
+  end
 end
 
 son = Child.new
 son.send :say_hi
+lad = Child.new
+las.send :say_hi
