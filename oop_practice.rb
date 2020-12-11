a091cab8a7a99637169fe5ab1a0625de588e4441
Poncho <jplyles1212@gmail.com>
2020-12-10 17:06:10 -0800

    Add a class variable to superclass to track number of objects created

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,39 +1,21 @@
 class Vehicle
+  @@number_of_vehicles = 0
+
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas"
   end
+
+  def print_value
+    puts @@number_of_vehicles
+  end
+  
 end
 
 class MyCar < Vehicle
   NUMBER_OF_DOORS = 4
+end
 
-  def initialize(year, model, color)
-    @year = year
-    @model = model
-    @color = color
-    @current_speed = 0
-  end
-
-  def speed_up(number)
-    @current_speed += number
-    puts "You push the gas and accelerate #{number} mph."
-  end
-
-  def brake(number)
-    @current_speed -= number
-    puts "You push the brake and decelerate #{number} mph."
-  end
-
-  def current_speed
-    puts "You are now going #{@current_speed} mph."
-  end
-
-  def shut_down
-    @current_speed = 0
-    puts "Let's park this bad boy!"
-  end
-
-  class myTruck < Vehicle
+  class MyTruck < Vehicle
     NUMBER_OF_DOORS = 2
   end
 end
