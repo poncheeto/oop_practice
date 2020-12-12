ab5e7c96cef8837ae8b396cbd51a1d6dea1ca54d
Poncho <jplyles1212@gmail.com>
2020-12-11 19:37:41 -0800

    Print method lookup for classes

--- oop_practice.rb
+++ oop_practice.rb
@@ -31,8 +31,6 @@ include Towable
   NUMBER_OF_DOORS = 2
 end
 
-car = MyCar.new
-car2 = MyTruck.new
-Vehicle.number_of_vehicles
-p car2.can_tow?(1500)
-p car.can_tow?(2500)
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
