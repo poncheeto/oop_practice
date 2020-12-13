dfda48003604dec253bc4153d94ae6536bf52399
Poncho <jplyles1212@gmail.com>
2020-12-12 16:41:50 -0800

    Move all related behaviors to superclass

--- oop_practice.rb
+++ oop_practice.rb
@@ -6,22 +6,44 @@ end
 
 class Vehicle
   @@number_of_vehicles = 0
-
-  def self.gas_mileage(gallons, miles)
-    puts "#{miles / gallons} miles per gallon of gas"
-  end
+  
 
   def initialize
     @@number_of_vehicles += 1
+    @current_speed = 0
   end
 
   def self.number_of_vehicles
     puts @@number_of_vehicles
   end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
 end
 
 class MyCar < Vehicle
 include Towable
+
   NUMBER_OF_DOORS = 4
 end
 
@@ -31,6 +53,9 @@ include Towable
   NUMBER_OF_DOORS = 2
 end
 
-puts MyCar.ancestors
-puts MyTruck.ancestors
-puts Vehicle.ancestors
+truck = MyTruck.new
+truck.speed_up(5)
+truck.current_speed
+truck.brake(5)
+truck.current_speed
+truck.shut_down
