ddfa5fd025a68c8db9b9c64b85ab9b02a49da367
Poncho <jplyles1212@gmail.com>
2020-12-13 17:35:57 -0800

    Create Student class

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,76 +1,19 @@
-module Towable
-  def can_tow?(pounds)
-    pounds < 2000 ? true : false
-  end
-end
+class Student
+  attr_accessor :name
 
-class Vehicle
-  attr_accessor :color
-  attr_reader :model, :year
-  @@number_of_vehicles = 0
-
-  def self.number_of_vehicles
-    puts "This program has created #{@@number_of_vehicles} vehicles"
+  def initialize(n, g)
+    @name = n
+    @grade = g
   end
 
-  def initialize(year, model, color)
-    @year = year
-    @model = model
-    @color = color
-    @current_speed = 0
-    @@number_of_vehicles += 1
-  end
-
-  def speed_up(number)
-    @current_speed += number
-    puts "You push the gas and accelerate #{number} mph."
-  end
-
-  def brake(number)
-    @current_speed -= number
-    puts "You push the brake and decelerate #{number} mph."
-  end
-
-  def current_speed
-    puts "You are now going #{@current_speed} mph."
-  end
-
-  def shut_down
-    @current_speed = 0
-    puts "Let's park this bad boy!"
-  end
-
-  def self.gas_mileage(gallons, miles)
-    puts "#{miles / gallons} miles per gallon of gas"
-  end
-
-  def spray_paint(color)
-    self.color = color
-    puts "Your new #{color} paint job looks great!"
-  end
-
-  def print_age
-    puts age_of_vehicle
-  end
+  def better_grade_than?()
 
   private
 
-  def age_of_vehicle
-    Time.now.year - self.year
+  def grade
+    @grade
   end
 end
 
-class MyCar < Vehicle
-include Towable
-
-  NUMBER_OF_DOORS = 4
-end
-
-class MyTruck < Vehicle
-include Towable
-
-  NUMBER_OF_DOORS = 2
-end
-
-truck = MyTruck.new(2013, "Ford", "black")
-truck.print_age
+poncho = Student.new("Poncho", 93)
+p poncho.grade
