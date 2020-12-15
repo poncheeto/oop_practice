9efe92084f3c073c78fe7781636ab01acee2cb94
Poncho <jplyles1212@gmail.com>
2020-12-14 16:37:36 -0800

    Add Viking class

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,24 +1,4 @@
-class Student
-  attr_accessor :name
-
-  def initialize(n, g)
-    @name = n
-    @grade = g
-  end
-
-  def better_grade_than?(name)
-    if grade > name.grade
-      puts "Well done!"
-    end
-  end
-
-  protected
-
-  def grade
-    @grade
+class Viking
+  def initialize(name, age, health, strength)
   end
 end
-
-poncho = Student.new("Poncho", 93)
-aubz = Student.new("Aubz", 75)
-poncho.better_grade_than?(aubz)
