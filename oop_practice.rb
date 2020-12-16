6cc50aedc4771f4ddfa3a1b67246e403f860f079
Poncho <jplyles1212@gmail.com>
2020-12-15 18:49:17 -0800

    Create new instance of Viking class

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,4 +1,11 @@
 class Viking
   def initialize(name, age, health, strength)
+    @name = name
+    @age = age
+    @health = health
+    @strength = strength
   end
 end
+
+oleg = Viking.new("Oleg", 19, 100, 8)
+p oleg
