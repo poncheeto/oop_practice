6472249661cbdeda84e580009646c2bb6bbce820
Poncho <jplyles1212@gmail.com>
2020-12-19 14:24:51 -0800

    Practice error handling with user input

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,16 +1,5 @@
-values = [42, 'a', 'r', 9, 5, 10022, 8.7, "sharon", "Libya", "Mars", "12", 98, rand + rand, {:dog=>'cat'}, 100, nil, 200.0000, Object, 680, 3.14, "Steve", 78, "Argo"].shuffle
-
-while values.length > 0
-  a = values.pop
-  b = values.pop
-
-  begin
-  a + b
-
-  rescue
-    puts "Could not add variable a (#{a.class}) and b (#{b.class})"
-    
-  else
-    puts "a + b is #{a + b}"
-  end
+while 1
+  puts "Enter a number>>"
+  num = Kernel.gets.match(/\d+/)[0]
+  puts "#{num} + 1 is: #{num+1}"
 end
