2eddc4c4dcd2ce6c17ee35dbbcf0f0eb071394b0
Poncho <jplyles1212@gmail.com>
2020-12-19 14:43:25 -0800

    Compare code when written as if/else block

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,10 +1,20 @@
+# while 1
+#   puts "Enter a number>>"
+#   begin
+#     num = Kernel.gets.match(/\d+/)[0]
+#   rescue
+#     puts "Erroneous input! Try again..."
+#   else
+#     puts "#{num} + 1 is: #{num.to_i+1}"
+#   end
+# end
+
 while 1
   puts "Enter a number>>"
-  begin
-    num = Kernel.gets.match(/\d+/)[0]
-  rescue
-    puts "Erroneous input! Try again..."
-  else
+  if num = Kernel.gets.match(/\d+/)
+    num = num[0]
     puts "#{num} + 1 is: #{num.to_i+1}"
+  else
+    puts "Erroneous input! Try again..."
   end
 end
