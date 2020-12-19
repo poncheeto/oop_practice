b75ab3223b6f0ccbe515426f9d6663d63ceb57a2
Poncho <jplyles1212@gmail.com>
2020-12-19 12:47:41 -0800

    Show how adding a number and a string will give error

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,30 +1,4 @@
-class Cinema
-  attr_accessor :name, :location
+a = 10
+b = "42"
 
-  def initialize(name, location)
-    @name = name
-    @location = location
-    @movies = []
-  end
-
-  def add_movie(movie)
-    @movies << movie
-    movie.cinema = self
-  end
-end
-
-class Movie
-  attr_accessor :title, :showtime, :cinema
-
-  @@all = []
-
-  def initialize(title, showtime)
-    @title = title
-    @showtimes = showtime
-    @@all << self
-  end
-end
-
-bow_tie = Cinema.new("BowTie Cinema", "Main St, Richmond")
-parasite = Movie.new("Parasite", "7:00")
-bow_tie.add_movie(parasite)
+a + b
