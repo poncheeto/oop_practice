adeccf1a69204275cc478f68c5907414f061fc4f
Poncho <jplyles1212@gmail.com>
2020-12-19 12:58:00 -0800

    Add array of many different value to test error handling

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,12 +1,16 @@
-a = 10
-b = "42"
+values = [42, 'a', 'r', 9, 5, 10022, 8.7, "sharon", "Libya", "Mars", "12", 98, rand + rand, {:dog=>'cat'}, 100, nil, 200.0000, Object, 680, 3.14, "Steve", 78, "Argo"].shuffle
 
-begin
-a + b
+while values.length > 0
+  a = values.pop
+  b = values.pop
 
-rescue
-  puts "Could not add variable a (#{a.class}) and b (#{b.class})"
-  
-else
-  puts "a + b is #{a + b}"
+  begin
+  a + b
+
+  rescue
+    puts "Could not add variable a (#{a.class}) and b (#{b.class})"
+    
+  else
+    puts "a + b is #{a + b}"
+  end
 end
