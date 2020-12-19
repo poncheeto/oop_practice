31e5af070b2795444cd51383853b3b5e3424577a
Poncho <jplyles1212@gmail.com>
2020-12-19 14:38:04 -0800

    Add begin/resue block to handle errors

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,5 +1,10 @@
 while 1
   puts "Enter a number>>"
-  num = Kernel.gets.to_i.to_s.match(/\d+/)[0]
-  puts "#{num} + 1 is: #{num.to_i+1}"
+  begin
+    num = Kernel.gets.match(/\d+/)[0]
+  rescue
+    puts "Erroneous input! Try again..."
+  else
+    puts "#{num} + 1 is: #{num.to_i+1}"
+  end
 end
