17f312b83932a5642bf4e8817ea9d8f02e726f11
Poncho <jplyles1212@gmail.com>
2020-12-22 13:52:10 -0800

    Add getter and setter methods and a method to print out full name

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,5 +1,22 @@
 class Person
-  def initialized(first_name, last_name)
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
   end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    puts (first_name + last_name)
+  end
+
+end
+
+loncho_piles = Person.new("Loncho", "Piles")
+loncho_piles.full_name
