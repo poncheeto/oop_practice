e52a70c490944779738e579ed6b0cb7f18433c40
Poncho <jplyles1212@gmail.com>
2020-12-22 08:36:58 -0800

    Create example that prints type of error thrown

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,29 +1,12 @@
-require 'open-uri'
-remote_base_url = "http://en.wikipedia.org/wiki"
-
-[1900, 1910, 'xj3490', 2000].each do |yr|
-
-  retries = 3
-
+while 1 
+  puts "Enter a number>>"
   begin
-    url = "#{remote_base_url}/#{yr}"
-    puts "Getting page #{url}"
-    rpage = open(url)
-  rescue StandardError=>each
-    puts "\tError: #{e}"
-    if retries > 0
-      puts "\rTrying #{retries} more times"
-      retries -= 1
-      sleep 1
-      retry
-    else
-      puts "\t\tCan't get #{yr}, so moving on"
-    end
-  else 
-    puts "\tGot page for #{yr}"
-  ensure
-    puts "Ensure branch; sleeping"
-    sleep 1
-
+    num = Kernal.gets.match(/\d+/)[0]
+  rescue StandardError=>e
+    puts "Erroneous input!"
+    puts e
+    puts "\tTry again...\n"
+  else
+    puts "#{num} + 1 is: #{num.to_i+1}"
   end
 end
