58f7376fb3a8824e9c163f314834780d789cb435
Poncho <jplyles1212@gmail.com>
2020-12-22 13:54:18 -0800

    Create module that take one number and raises it to power of second number

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,22 +1,6 @@
-class Person
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+module MathHelpers
+  def exponent(a, b)
+    a ** b
   end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def full_name
-    puts (first_name + last_name)
-  end
-
 end
 
-loncho_piles = Person.new("Loncho", "Piles")
-loncho_piles.full_name
