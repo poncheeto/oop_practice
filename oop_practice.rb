a5ab4c93516e9c18c0905e500ee4759aec4d2323
Poncho <jplyles1212@gmail.com>
2020-12-22 13:30:47 -0800

    Add methods to calculate batting average and on base percentage

--- oop_practice.rb
+++ oop_practice.rb
@@ -4,4 +4,16 @@ class BaseballPlayer
     @walks = walks
     @at_bats = at_bats
   end
+
+  def batting_average
+    (@hits.to_f / @at_bats)
+  end
+
+  def on_base_percentage
+    ((@hits.to_f + @walks) / @at_bats)
+  end
 end
+
+player = BaseballPlayer.new(5, 2, 31)
+p player.batting_average
+p player.on_base_percentage
