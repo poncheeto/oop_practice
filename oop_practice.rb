ef05d9d49e0f0fc398d7134fae847a6dfdab790b
Poncho <jplyles1212@gmail.com>
2020-12-22 13:47:43 -0800

    Create Person class that initializes with a first and last name

--- oop_practice.rb
+++ oop_practice.rb
@@ -1,19 +1,5 @@
-class BaseballPlayer
-  def initialize(hits, walks, at_bats)
-    @hits = hits
-    @walks = walks
-    @at_bats = at_bats
+class Person
+  def initialized(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
   end
-
-  def batting_average
-    (@hits.to_f / @at_bats)
-  end
-
-  def on_base_percentage
-    ((@hits.to_f + @walks) / @at_bats)
-  end
-end
-
-player = BaseballPlayer.new(5, 2, 31)
-p player.batting_average
-p player.on_base_percentage
