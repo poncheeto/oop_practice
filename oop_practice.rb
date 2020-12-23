f200bc6d17629458b5c6459770d85efa0fc90d43
Poncho <jplyles1212@gmail.com>
2020-12-23 08:45:15 -0800

    Create 3 descriptive variable names and simple name in loop

--- oop_practice.rb
+++ oop_practice.rb
@@ -2,12 +2,10 @@
 
 name = "Jane"
 placeOfBirth = "Bratislava"
-placeOfBirth = "Kosice"
-favorite_season = "autumn"
+occupation = "student"
 
-n1 = 2
-n2 = 4
-n3 = 7
-
-p name, placeOfBirth, favorite_season
-p n1, n2, n3
+i = 5
+while i > 0 do
+  puts name
+  i -= 1
+end
